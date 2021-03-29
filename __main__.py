c7eda49968ed4128b672fc19a08f3489ef95122d
Nathan Wilson <n8.doubleu@gmail.com>
2021-03-29 03:40:53 +0000

    Variable names now Zen, workflow choice add

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 import requests, json
 from PIL import Image
+from pathlib import Path
 
 def main():
 	#Load current API keys
@@ -7,38 +8,37 @@ def main():
 		keys = json.load(file)
 	#Greet User
 	print("Welcome to Python API Wrapper.")
+	command = {}
 	print("	Available Commands: ")
-
-	APOD = get_APOD(keys['NASA'])
+	print("	1. Astronomy Photo of the Day")
+	command['1'] = get_apod
+	print("	2. Download Perseverance Photo Album")
+	command['2'] = perseverance	
+	command[input("Enter Choice: ")](keys['NASA'])
 	
-	for key in APOD.keys():
-		print(key+': '+APOD[key])
-
-	
-def get_APOD(NASA_key):
+def get_apod(NASA_key):
 	print('Getting Astronomy Photo of the Day...')
 	r = requests.get('https://api.nasa.gov/planetary/apod'+'?api_key='+NASA_key)
 	print("Request status code " + str(r.status_code))
-	APOD = r.json()
-	return APOD
+	apod = r.json()
+	for key in apod.keys():
+		print(key+': '+apod[key])
+	return apod
 
-def add_key(key, value):
-	with open('Keys.json','r') as file:
-		keys = json.load(file)
-	
-	keys[key] = value
-
-	with open('Keys.json', 'r') as file:
-		json.dump(keys, file)
-
-def remove_key(key):
-	with open('Keys.json','r') as file:
-		keys = json.load(file)
-	
-	del keys[key]
-
-	with open('Keys.json', 'r') as file:
-		json.dump(keys, file)
+def perseverance(NASA_key):
+	print("Perseverance photos")
+	sol = int(input("(1- 18) SOL: "))
+	path ='SOL'+str(sol).zfill(2)+'/'
+	Path(path).mkdir(parents=True, exist_ok=True)
+	r = requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/Perseverance/photos?sol='+str(sol)+'&api_key='+NASA_key)
+	photo_directory = r.json()
+	n = len(photo_directory["photos"])
+	print("Photos: "+str(n))
+	for page in range(0, n):
+		image_url = photo_directory['photos'][page]["img_src"]
+		image = Image.open(requests.get(image_url,stream = True).raw)
+		image.save(path+'/SOL-'+str(sol).zfill(2)+'page-'+str(page).zfill(len(str(n)))+'.jpg')
+	return 
 
 if __name__=='__main__':
 	main()
